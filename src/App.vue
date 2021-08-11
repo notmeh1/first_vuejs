924bf02fef038feae0164956f139bd6eb1dfab50
Andres <zleckzxd@gmail.com>
2021-08-11 01:49:15 -0400

    + stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <h1 class="text-center">Tabla de Usuarios</h1>
+    <table id="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">RUN</th>
+          <th scope="col">Nacimiento</th>
+          <th scope="col">Edad</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(persona, $index) of personas" :key="$index">
+          <th scope="row">{{ $index }}</th>
+          <td>{{ personas.nombre }}</td>
+          <td>{{ personas.apellido }}</td>
+          <td>{{ personas.run }}</td>
+          <td>{{ personas.nacimiento }}</td>
+          <td>{{ personas.edad }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
+const table = new Vue({
+  el: "#table",
+  data: {
+    personas: [
+      {
+        nombre: "Jose",
+        apellido: "Pérez",
+        run: "123456-2",
+        nacimiento: "12-04-67",
+        edad: 52,
+      },
+      {
+        nombre: "María",
+        apellido: "Rodríguez",
+        run: "654321-1",
+        nacimiento: "30-11-90",
+        edad: 29,
+      },
+      {
+        nombre: "Manuel",
+        apellido: "Patiño",
+        run: "14223456-6",
+        nacimiento: "02-12-59",
+        edad: 60,
+      },
+    ],
   },
-};
+});
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
